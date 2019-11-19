3c56a0356db51bea207ebab8235032b97f7c535d
travis-john <travis.wl.john@gmail.com>
2019-11-18 20:29:45 -0500

    password generation js

---
+++ assets/js/password-gen.js
@@ -0,0 +1,43 @@
+// function generatePassword() {
+//     var passwordLength = prompt('How many characters?');
+//     var specialCharacters = confirm('Include special characters?');
+//     var numericCharacters = confirm('Include numbers?');
+//     var upperCaseCharacters = confirm('Include upper case characters');
+//     var lowerCaseCharacters = confirm('Include lower case characters');   
+// }
+
+document.getElementById('generate').addEventListener('click', function(){
+    var passwordLength = prompt('How many characters?');
+    var specialCharacters = confirm('Include special characters?');
+    var numericCharacters = confirm('Include numbers?');
+    var upperCaseCharacters = confirm('Include upper case characters');
+    var lowerCaseCharacters = confirm('Include lower case characters');
+    var characterPool = '';
+    password = "";
+
+    if (specialCharacters) {
+        specialCharacters = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+        characterPool += specialCharacters;
+    }
+    if (numericCharacters) {
+        numericCharacters = '0123456789';
+        characterPool += numericCharacters;
+    }
+    if (lowerCaseCharacters) {
+        lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+        characterPool += lowerCaseCharacters;
+    }
+    if (upperCaseCharacters) {
+        upperCaseCharacters = lowerCaseCharacters.toUpperCase();
+        characterPool += upperCaseCharacters;
+    }
+    console.log(characterPool);
+    
+    for(var i = 0; i <  passwordLength; i ++){
+        password += characterPool.charAt(Math.floor(Math.random() * characterPool.length));
+        return password;
+    }
+
+});
+
+
